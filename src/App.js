aa216b8cbaece1f93bf8e04084db5b655408589c
Tyler Lombardi <tylombardi@gmail.com>
2024-04-06 12:01:04 -0400

    added the routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import Routing from './Routing';
 import { Orders } from './views';
 import { Header } from './components';
 import './styles/global.scss';
 
 const App = () => {
-  const [headerText, setHeaderText] = useState('asf');
+
+  const [headerText, setHeaderText] = useState();
 
   return (
     <>
       <Header text={headerText}></Header>
-      <Orders
-        headerText={headerText}
-        setHeaderText={setHeaderText}
-      ></Orders>
+      <Routing
+        defaultView={<Orders
+          setHeaderText={setHeaderText} />}
+      />
     </>
   );
 };
